e0fe3a09e675f4fdebe635288a3b53b39798fe7f
Connor Cowan <ccowan003@comcast.net>
2019-05-16 00:10:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
 
-  if (integer % 3 == 0)
+  if (integer % 3 == 0 && integer % 5 == 0) #must do these first! then other divisibles next!
     return "Fizz"
   elsif (integer % 5  == 0)
     return "Buzz"
-  elsif (integer % 3 == 0 && integer % 5 == 0)
+  elsif (integer % 3 == 0)
     return "FizzBuzz"
   end
 end
