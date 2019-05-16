85b8bd35fedafc2a640fdf06896f6288b5e0a136
Connor Cowan <ccowan003@comcast.net>
2019-05-16 00:09:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(integer)
 
   if (integer % 3 == 0)
-    return "FizzBuzz"
+    return "Fizz"
   elsif (integer % 5  == 0)
     return "Buzz"
   elsif (integer % 3 == 0 && integer % 5 == 0)
